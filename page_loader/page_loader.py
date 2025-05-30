91efefefe318c1c60948fc1fd877a88651bd307b
DanikDreamer <skorodumovdg@gmail.com>
2025-05-30 16:02:03 +0300

    Revert "f"
    This reverts commit 79db6adfe1e9e993344eb903d2f696b308cfbd5f.

--- page_loader/page_loader.py
+++
@@ -1,7 +0,0 @@
-from .download_manager import DownloadManager
-
-
-def download(url, path=None):
-    path = path or ''
-    page_loader = DownloadManager(url, path)
-    return page_loader.download()
