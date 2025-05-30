6745ae86808915e72034af949b900006f3c45a63
DanikDreamer <skorodumovdg@gmail.com>
2025-05-30 08:41:25 +0300

    Fixed

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -41,6 +41,8 @@ def test_storage_errors(requests_mock, tmp_path):
     url = "https://site.com/blog/about"
     requests_mock.get(url)
 
+    assert requests_mock.called
+
     with pytest.raises((OSError, PermissionError)):
         download(url, "/sys")
 
