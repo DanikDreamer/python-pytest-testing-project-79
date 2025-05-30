79db6adfe1e9e993344eb903d2f696b308cfbd5f
DanikDreamer <skorodumovdg@gmail.com>
2025-05-30 15:40:25 +0300

    f

--- tests/fixtures/test_files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def file_fixtures(file):
-    if "code" in os.listdir(os.getcwd()):
-        file_path = os.path.join("code", "tests/fixtures", file)
-    else:
-        file_path = os.path.join("tests/fixtures", file)
-    return file_path
